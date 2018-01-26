1f7d9b38eb59e9a2f3801d0102895abeeaa3d688
sather33 <court.dream3@gmail.com>
2018-01-26 15:34:09 +0800

    change instance to local variable

--- check.rb
+++ check.rb
@@ -6,23 +6,23 @@ load 'load/clean-csv.rb'
 
 
 @items.each do |item|
-  @item_array=[]
+  item_array=[]
   urls = URI.extract(item.description, ['http', 'https'])
   urls.each do |url|
     puts "#{url}  "
     unless fetch(url) == "200"
-      if @item_array.empty?
-        @item_array << item.title
-        @item_array << "https://www.youtube.com/watch?v=#{item.video_id}"
+      if item_array.empty?
+        item_array << item.title
+        item_array << "https://www.youtube.com/watch?v=#{item.video_id}"
       end
-      @item_array << url
+      item_array << url
       puts "bad"
     end
   end
-  unless @item_array.empty?
-    @item_array << " "
+  unless item_array.empty?
+    item_array << " "
     CSV.open('csv/check.csv', 'a') do |csv|
-      csv << @item_array
+      csv << item_array
     end
   end
 end
